85efa93ec465fa03bf7c79893e246122ebef6974
Bykovskikh Evgeny <e.bykovskikh@rambler-co.ru>
2018-11-22 23:32:11 +0300

    Add airbnb linting

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // vendors
 import 'bulma/css/bulma.min.css';
 
-import App from './components/App'
+import App from './components/App';
 
 // React 17 will replace render for server side by hydrate
 if (process.env.NODE_ENV !== 'production') {
-  ReactDOM.render(<App/>, document.getElementById('root'))
+  ReactDOM.render(<App />, document.getElementById('root'));
 } else {
-  ReactDOM.hydrate(<App/>, document.getElementById('root'))
+  ReactDOM.hydrate(<App />, document.getElementById('root'));
 }
